e55ad81cb841a39d5fa15af973869c07dbbdda18
alan nungaray <alan.nungaray@softtekclients.com>
2017-08-11 09:56:15 -0700

    webpack import static files

--- webpack/project/src/index.js
+++ webpack/project/src/index.js
@@ -1,8 +1,11 @@
-import _ from 'lodash'
-import mycomponent from './mycomponent.js'
+import _ from 'lodash';
+import mycomponent from './mycomponent.js';
+import './style.css';
+import icon from './icon.png'
 function component() {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('red')
   return element;
 }
 
